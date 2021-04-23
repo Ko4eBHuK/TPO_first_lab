4f5513c3f31e1a69965b9880718fa06a99eb3458
Ko4eBHuK <okladnikov.bool@gmail.com>
2021-04-23 17:53:03 +0300

    intermediate test has been added

--- src/main/kotlin/sorting/CountingSort.kt
+++ src/main/kotlin/sorting/CountingSort.kt
@@ -1,6 +1,8 @@
 package sorting
 
 class CountingSort {
+    lateinit var arrayOfCountsFinal: Array<Long>
+
     fun sort(arr: Array<Long>): Array<Long>{
         val arrayOfCounts = Array<Long>((arr.maxOrNull() ?: 0).toInt() + 1, { i -> 0})
         //подсчёт количества каждого элемента входящего массива
@@ -9,6 +11,8 @@ class CountingSort {
         //определение количества чисел меньших чем текущее
         for(i in 1 until arrayOfCounts.size)
             arrayOfCounts[i] += arrayOfCounts[i-1]
+
+        this.arrayOfCountsFinal = Array<Long>(arrayOfCounts.size, { i -> arrayOfCounts[i]})
 
         val resultArray = Array<Long>(arr.size, { i -> 0})
         var k = arr.size - 1
--- src/test/kotlin/SortingTest.kt
+++ src/test/kotlin/SortingTest.kt
@@ -1,28 +1,41 @@
+import org.junit.jupiter.api.*
 import sorting.CountingSort
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-
 class SortingTest {
-    private val sorter: CountingSort = CountingSort()
-    private val originArray: Array<Long> = arrayOf(13, 15, 18, 28, 12, 14, 11, 25, 11, 27, 24, 9, 15, 1, 15, 24, 6, 21, 0, 8, 24, 13, 15, 19, 13, 1, 13, 15, 17, 21)
-    private val expectedSortedArray: Array<Long> = arrayOf(0, 1, 1, 6, 8, 9, 11, 11, 12, 13, 13, 13, 13, 14, 15, 15, 15, 15, 15, 17, 18, 19, 21, 21, 24, 24, 24, 25, 27, 28)
+    private lateinit var actualSortedArray: Array<Long>
+    private lateinit var testingCountingArray: Array<Long>
 
-    //TODO(протестировать промежуточную точку когда значения в количественном массиве выставятся в индексы результирующего)
+    @BeforeEach
+    fun initializeActualSortedArray(){
+        val sorter = CountingSort()
+        actualSortedArray = sorter.sort(originArray)
+        testingCountingArray = sorter.arrayOfCountsFinal
+    }
 
     @Test
     @DisplayName("Manual sort testing")
     fun testManual(){
-        val testedSortedArray = sorter.sort(originArray)
-        Assertions.assertArrayEquals(expectedSortedArray, testedSortedArray)
+        //val testedSortedArray = sorter.sort(originArray)
+        Assertions.assertArrayEquals(expectedSortedArray, actualSortedArray)
     }
 
     @Test
     @DisplayName("Machine sort testing")
     fun testByComputer(){
-        val testedSortedArray = sorter.sort(originArray)
+        //val testedSortedArray = sorter.sort(originArray)
         val sortedByMachineArray = originArray.sortedArray()
-        Assertions.assertArrayEquals(sortedByMachineArray, testedSortedArray)
+        Assertions.assertArrayEquals(sortedByMachineArray, actualSortedArray)
+    }
+
+    @Test
+    @DisplayName("intermediate point testing: ready-made counting array")
+    fun testCountingArray(){
+        Assertions.assertArrayEquals(expectedCountingArray, testingCountingArray)
+    }
+
+    companion object{
+        private val originArray: Array<Long> = arrayOf(13, 15, 18, 28, 12, 14, 11, 25, 11, 27, 24, 9, 15, 1, 15, 24, 6, 21, 0, 8, 24, 13, 15, 19, 13, 1, 13, 15, 17, 21)
+        private val expectedSortedArray: Array<Long> = arrayOf(0, 1, 1, 6, 8, 9, 11, 11, 12, 13, 13, 13, 13, 14, 15, 15, 15, 15, 15, 17, 18, 19, 21, 21, 24, 24, 24, 25, 27, 28)
+        private val expectedCountingArray: Array<Long> = arrayOf(1, 3, 3, 3, 3, 3, 4, 4, 5, 6, 6, 8, 9, 13, 14, 19, 19, 20, 21, 22, 22, 24, 24, 24, 27, 28, 28, 29, 30)
     }
 }
