e2edb59ba2a7adc706d6d13341125ac91fbbab5b
Ko4eBHuK <okladnikov.bool@gmail.com>
2021-04-23 16:43:20 +0300

    parameterized tests has been created in FunctionTest

--- src/test/kotlin/FunctionTest.kt
+++ src/test/kotlin/FunctionTest.kt
@@ -1,16 +1,16 @@
 import function.Calculator
 
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 import kotlin.math.*
 
 class FunctionTest {
     private val testCalc: Calculator = Calculator()
-
-        //TODO("параметризовать тесты при джюнит5")
 
     @Test
     @DisplayName("test in zero point")
@@ -20,37 +20,20 @@ class FunctionTest {
         assertThat(testedAnswer).isEqualTo(correctAnswer)
     }
 
-    @Test
-    @DisplayName("test near zero positive")
-    fun testSecantNearZeroPositive(){
-        val correctAnswer = 1/cos(0.00008)
-        val testedAnswer = testCalc.secant(0.00008)
-        assertThat(testedAnswer).isEqualTo(correctAnswer)
+    @ParameterizedTest
+    @ValueSource(doubles = [0.00008, -0.00008])
+    fun testSecantNearZero(input: Double){
+        val expectedAnswer = 1/cos(input)
+        val actualAnswer = testCalc.secant(input)
+        assertThat(actualAnswer).isEqualTo(expectedAnswer)
     }
 
-    @Test
-    @DisplayName("test near zero negative")
-    fun testSecantNearZeroNegative(){
-        val correctAnswer = 1/cos(-0.00008)
-        val testedAnswer = testCalc.secant(-0.00008)
-        assertThat(testedAnswer).isEqualTo(correctAnswer)
-    }
-
-    @Test
-    @DisplayName("test near values positive")
-    fun testNearValuesPositive(){
-        val correctAnswer = 1/cos(0.05)
-        val testedAnswer = testCalc.secant(0.05)
-        assertThat(testedAnswer).isCloseTo(correctAnswer, offset(0.001))
-        //assertEquals(testedAnswer, correctAnswer, 0.001)
-    }
-
-    @Test
-    @DisplayName("test near values negative")
-    fun testNearValuesNegative(){
-        val correctAnswer = 1/cos(-0.05)
-        val testedAnswer = testCalc.secant(-0.05)
-        assertThat(testedAnswer).isCloseTo(correctAnswer, offset(0.001))
+    @ParameterizedTest
+    @ValueSource(doubles = [0.05, -0.05])
+    fun testSecantNear(input: Double){
+        val expectedAnswer = 1/cos(input)
+        val actualAnswer = testCalc.secant(input)
+        assertThat(actualAnswer).isCloseTo(expectedAnswer, offset(0.001))
     }
 
     @Test
